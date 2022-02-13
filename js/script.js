357da38eebf8fc439dff86e2f275f2f1f03c11c6
Tukashov <erbol.tukashov.kk@gmail.com>
2022-02-13 21:41:43 +0600

    feat: add task todo list✅

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const form = document.querySelector('#add-todo');
+const input = document.querySelector('#input-todo');
+const btnSubmit = document.querySelector('button');
+const todoList = document.querySelector('.todo-list');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+});
+
+btnSubmit.addEventListener('click', function (e) {
+    let todo = document.createElement('div');
+    todo.classList.add('todo-item');
+    todoList.appendChild(todo);
+    todo.innerHTML = input.value;
+    input.value = '';
+
+    todo.addEventListener('click', () => {
+        todo.remove()
+    });
+});
