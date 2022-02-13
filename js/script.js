497c26f304de7f77301ef4bd7ea050a6eabf3a6d
Tukashov <erbol.tukashov.kk@gmail.com>
2022-02-13 21:56:49 +0600

    refactor: change structure code JS

--- js/script.js
+++ js/script.js
@@ -1,20 +1,22 @@
-const form = document.querySelector('#add-todo');
-const input = document.querySelector('#input-todo');
-const btnSubmit = document.querySelector('button');
-const todoList = document.querySelector('.todo-list');
+window.addEventListener('load', function () {
+    const form = document.querySelector('#add-todo');
+    const input = document.querySelector('#input-todo');
+    const btnSubmit = document.querySelector('button');
+    const todoList = document.querySelector('.todo-list');
 
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-});
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+    });
 
-btnSubmit.addEventListener('click', function (e) {
-    let todo = document.createElement('div');
-    todo.classList.add('todo-item');
-    todoList.appendChild(todo);
-    todo.innerHTML = input.value;
-    input.value = '';
+    btnSubmit.addEventListener('click', function (e) {
+        let todo = document.createElement('div');
+        todo.classList.add('todo-item');
+        todoList.appendChild(todo);
+        todo.innerHTML = input.value;
+        input.value = '';
 
-    todo.addEventListener('click', () => {
-        todo.remove()
+        todo.addEventListener('click', () => {
+            todo.remove()
+        });
     });
 });
